17f8a9235b1f2b775de048c97b8187534693b1c8
WildWoz <adam.wozy@gmail.com>
2020-03-15 13:17:25 +1100

    Initial commit of all gradebook files and tests

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,25 @@
+﻿using GradeBook;
+using System;
+using System.Collections.Generic;
+
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Adam's Grade Book");
+            book.AddGrade(7.8);
+            book.AddGrade(72.86);
+            book.AddGrade(57.28);
+            book.GetStatistics();
+
+            var stats = book.GetStatistics();
+            Console.WriteLine($"Average: {stats.Average:N1}");
+            Console.WriteLine($"Lowest: {stats.Low:N1}");
+            Console.WriteLine($"Highest: {stats.High:N1}");
+
+        }
+    }
+}
---
+++ gradebook/test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void BookCalculatesAnAverageGrade()
+        {
+            // arrange
+            var book = new Book("");
+            book.AddGrade(12.2);
+            book.AddGrade(43.2);
+            book.AddGrade(15.6);
+
+            // act
+            var result = book.GetStatistics();
+
+            // assert
+            Assert.Equal(23.8, result.Average, 1);
+            Assert.Equal(43.2, result.High);
+            Assert.Equal(12.2, result.Low);
+
+        }
+    }
+}
