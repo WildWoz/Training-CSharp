f7d9577e00baac7e1d9f8b266e4f831a86f4f307
WildWoz <adam.wozy@gmail.com>
2020-04-04 17:14:25 +1100

    Still trying to test HTTP client for Bitbucket

--- HttpClientTest/Program.cs
+++ HttpClientTest/Program.cs
@@ -5,32 +5,23 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace HttpClientTest
 {
     class Program
     {
         static async Task Main(string[] args)
         {
+            var client_id = "gWS7RFBJTwsWS5Yrva";
+            
 
+
+        var client = new HttpClient();
+        var content = new StringContent("?client_id=gWS7RFBJTwsWS5Yrva&response_type=code");
+        var response = await client.PostAsync("https://bitbucket.org/site/oauth2/access_token",content);
+
+        Console.WriteLine(response);
         }
 }
-
-
-
 }
-
-        // var client = new HttpClient();
-        // var request = new HttpRequestMessage(new HttpMethod("POST"), "https://bitbucket.org/site/oauth2/access_token");
-        // var base64authorization = Convert.ToBase64String(Encoding.ASCII.GetBytes("gWS7RFBJTwsWS5Yrva:ydEdAPe6a7LAywzaWdkCxd9Qav7LDCmx"));
-        // request.Headers.TryAddWithoutValidation("Authorization", $"Basic {base64authorization}"); 
-
-        // var contentList = new List<string>();
-        // contentList.Add("grant_type=authorization_code");
-        // contentList.Add("code={code}");
-        // request.Content = new StringContent(string.Join("&", contentList));
-        // request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded"); 
-
-        // var response = await client.SendAsync(request);
-
-        // Console.WriteLine(response);
 
