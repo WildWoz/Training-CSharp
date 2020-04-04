f07bdffe73c34764d0d3f10099dec4e5df72bc21
WildWoz <adam.wozy@gmail.com>
2020-04-04 23:31:48 +1100

    Continued HttpCLient testing

--- HttpClient_ConsoleApp/HttpClient_ConsoleApp/Program.cs
+++ HttpClient_ConsoleApp/HttpClient_ConsoleApp/Program.cs
@@ -13,25 +13,25 @@ namespace HttpClientTest
         static HttpClient client = new HttpClient();
         static async Task Main(string[] args)
         {
-        await PostRequestAsync("https://bitbucket.org/site/oauth2/authorize?client_id=gWS7RFBJTwsWS5Yrva&response_type=code","");
-        Console.WriteLine("Hello");
+            await PostRequestAsync("https://bitbucket.org/site/oauth2/authorize?client_id=gWS7RFBJTwsWS5Yrva&response_type=code", "");
+            Console.WriteLine("Hello");
         }
 
         public static async Task<string> PostRequestAsync(String uri, String parameters)
         {
             Console.WriteLine("Testing1");
-            //var client_id = "gWS7RFBJTwsWS5Yrva";
+            var client_id = "gWS7RFBJTwsWS5Yrva";
             //parameters = "";
             //uri = "https://bitbucket.org/site/oauth2/access_token";
-            var authUrl = "https://bitbucket.org/site/oauth2/authorize?client_id=gWS7RFBJTwsWS5Yrva&response_type=code";
+            var authUrl = "https://bitbucket.org/site/oauth2/authorize?client_id=" + client_id + "&response_type=code";
             System.Diagnostics.Process.Start(authUrl);
             var response = await client.PostAsync(uri, new StringContent(parameters));
             var content = await response.Content.ReadAsStringAsync();
             Console.WriteLine("Testing2: " + content);
-            
-            
+
+
             return content;
         }
-}
+    }
 }
 
